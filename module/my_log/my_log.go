2f8838236df599c23c635e9468512c98ed0b400c
XeiTongXueFlyMe <2067057918@qq.com>
2020-07-09 11:06:11 +0800

    修复bug

--- module/my_log/my_log.go
+++ module/my_log/my_log.go
@@ -17,50 +17,50 @@ type L struct {
 
 func (t *L) openFile() {
 	var err error
-	t.one.Do(func() {
-		t.fileName = ""
-		t.f = nil
-	})
 
 	year, month, _ := time.Now().Date()
-	if t.fileName != fmt.Sprintf("%d-%d.log", year, month) {
-		if t.f != nil {
-			t.f.Close()
-		}
+	t.fileName = fmt.Sprintf("%d-%d.log", year, month)
 
-		t.fileName = fmt.Sprintf("%d-%d.log", year, month)
-		t.f, err = os.OpenFile(t.fileName, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0777)
-		if err != nil {
-			panic(err)
-		}
-		t.logger = log.New(t.f, "", log.LstdFlags|log.Lshortfile)
+	t.f, err = os.OpenFile(t.fileName, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0777)
+	if err != nil {
+		panic(err)
 	}
+	t.logger = log.New(t.f, "", log.LstdFlags|log.Lshortfile)
+}
 
+func (t *L) closeFile() {
+	t.f.Close()
 }
 
 func (t *L) PrintfErr(format string, v ...interface{}) {
 	t.openFile()
 	t.logger.Output(2, "[err]"+fmt.Sprintf(format, v...))
+	t.closeFile()
 }
 func (t *L) PrintfWarring(format string, v ...interface{}) {
 	t.openFile()
 	t.logger.Output(2, "[warring]"+fmt.Sprintf(format, v...))
+	t.closeFile()
 }
 func (t *L) PrintfInfo(format string, v ...interface{}) {
 	t.openFile()
 	t.logger.Output(2, "[info]"+fmt.Sprintf(format, v...))
+	t.closeFile()
 }
 func (t *L) PrintlnErr(v ...interface{}) {
 	t.openFile()
 	t.logger.Output(2, "[err]"+fmt.Sprintln(v...))
+	t.closeFile()
 }
 func (t *L) PrintlnWarring(v ...interface{}) {
 	t.openFile()
 	t.logger.Output(2, "[warring]"+fmt.Sprintln(v...))
+	t.closeFile()
 }
 func (t *L) PrintlnInfo(v ...interface{}) {
 	t.openFile()
 	t.logger.Output(2, "[info]"+fmt.Sprintln(v...))
+	t.closeFile()
 }
 
 func (t *L) Printftml(format string, v ...interface{}) {
